2c9c1a59b0f3b48172cea1c84b1fa6ae3b78006b
Mihai <trofimciuc.m@gmail.com>
2022-06-03 10:44:46 +0300

    Added map array method for Card component

--- src/App.js
+++ src/App.js
@@ -3,27 +3,28 @@ import Card from "./components/Card";
 import contacts from "./contacts";
 import Avatar from "./components/Avatar";
 
-function App() {
-   const myPhoto = "https://www.californiamuseum.org/sites/main/files/imagecache/" +
-       "lightbox/main-images/cahalloffame_inductee_stevenspielberg_1800x2560.png";
-  return (
-      <div>
-          <h1 className="heading">My Contacts</h1>
-          <Avatar img={myPhoto} />
-          <Card
-            name={contacts[0].name}
-            img={contacts[0].imgURL}
-            tel={contacts[0].phone}
-            email={contacts[0].email}
-        />
+function createCard(contact) {
+    return (
         <Card
-            name={contacts[1].name}
-            img={contacts[1].imgURL}
-            tel={contacts[1].phone}
-            email={contacts[1].email}
+            key={contact.id}
+            name={contact.name}
+            img={contact.imgURL}
+            tel={contact.phone}
+            email={contact.email}
         />
-      </div>
-  );
+    );
+}
+
+function App() {
+    const myPhoto = "https://www.californiamuseum.org/sites/main/files/imagecache/" +
+        "lightbox/main-images/cahalloffame_inductee_stevenspielberg_1800x2560.png";
+    return (
+        <div>
+            <h1 className="heading">My Contacts</h1>
+            <Avatar img={myPhoto}/>
+            {contacts.map(createCard)}
+        </div>
+    );
 }
 
 export default App;
